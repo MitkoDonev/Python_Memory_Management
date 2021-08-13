7698d971b68fe0f202649f5fc571e09cf585bbc8
MDonev <donev90@gmail.com>
2021-08-13 11:56:32 +0300

    instance

--- CyclicalReference.py
+++ CyclicalReference.py
@@ -24,11 +24,3 @@ print(
 root.child(left)
 left.child(right)
 right.child(left)
-
-new_root_ref_count = sys.getrefcount(id(root))
-new_left_ref_count = sys.getrefcount(id(left))
-new_right_ref_count = sys.getrefcount(id(right))
-
-print(
-    f"Reference count:\nROOT: {new_root_ref_count}\nLEFT: {new_left_ref_count}\nRIGHT: {new_right_ref_count}"
-)
